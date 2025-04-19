ada73e93fbd1fb46c3e27bc4463b24a677c2f7b7
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-04-19 16:46:48 +0530

    update

--- Expo App/App1/App.js
+++ Expo App/App1/App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  let [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{count}</Text>
+      <Button style={styles.btn} onPress={() => setCount(count + 1)} title="Inc" />
+      <Button style={styles.btn} onPress={() => setCount(count - 1)} title="Dec" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "green",
+    backgroundColor: "black",
+    fontSize: 40,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderRadius: 10,
+    fontWeight: 600,
+  },
+  btn:{
+    padding:30,
+    margin:10,
+    backgroundColor:"blue",
   },
 });
---
+++ React Native/first_App/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Text,View} from "react-native"
+
+const Home=()=>{
+    return(
+        <>
+        <View>
+        <Text style={{}}>This is a My home</Text>
+        </View>
+        </>
+    )
+}
+
+export default Home
